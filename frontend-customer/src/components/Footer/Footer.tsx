13d49ab0963cbe7ab4185ea677b7c3d214f10798
YudBellingham <quanduysoma1@gmail.com>
2026-01-28 14:37:38 +0700

    header footer homepage customer ui

---
+++ frontend-customer/src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer style={{
+      background: '#eee',
+      padding: '16px',
+      marginTop: '40px',
+      textAlign: 'center'
+    }}>
+      © 2026 Lamtra Milk Tea
+    </footer>
+  )
+}
+
+export default Footer
